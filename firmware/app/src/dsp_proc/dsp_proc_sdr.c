8ece97bdedc700562fd2bab717b969151f5f07c9
Andrii Bilynskyi <andriy.bilynskyy@gmail.com>
2020-12-01 22:52:24 +0200

    Improve AM DC offset calculation.

--- firmware/app/src/dsp_proc/dsp_proc_sdr.c
+++ firmware/app/src/dsp_proc/dsp_proc_sdr.c
@@ -358,6 +358,9 @@ static inline void dsp_proc_sdr_modem(volatile app_handle_t * app_handle) {
         if(dsp_proc_sdr.transmission) {
             memset(dsp_proc_sdr.fft_buf, 0, sizeof(complex_f32_t) * (codec_buf_elements << 1));
         } else {
+            static float32_t middle_value = 0;
+            float32_t middle_alpha = 2000.f / ((float)dsp_proc_sdr_sr[app_handle->settings->codec_samplerate] * app_handle->settings->sdr_agc_tmieout_ms + 1000);
+
             uint16_t index_hi = ((uint32_t)app_handle->settings->sdr_bpf_am.high_hz * (codec_buf_elements << 1)) / dsp_proc_sdr_sr[app_handle->settings->codec_samplerate];
             for(uint16_t i = index_hi + 1; i < (codec_buf_elements << 1) - index_hi; i++) {
                 dsp_proc_sdr.fft_buf[i].re = 0;
@@ -365,20 +368,15 @@ static inline void dsp_proc_sdr_modem(volatile app_handle_t * app_handle) {
             }
             arm_cfft_f32(&dsp_proc_sdr.cfft, (float32_t *)dsp_proc_sdr.fft_buf, 1, 1);
 
-            float32_t max = 0, min = FLT_MAX;
             for(uint16_t i = 0; i < (codec_buf_elements << 1); i++) {
                 float32_t tmp;
                 (void)arm_sqrt_f32(dsp_proc_sdr.fft_buf[i].re * dsp_proc_sdr.fft_buf[i].re + dsp_proc_sdr.fft_buf[i].im * dsp_proc_sdr.fft_buf[i].im, &tmp);
-                if(tmp > max) {
-                    max = tmp;
-                } else if(tmp < min) {
-                    min = tmp;
-                }
+                middle_value = middle_alpha * tmp + (1.0f - middle_alpha) * middle_value;
                 dsp_proc_sdr.fft_buf[i].re = tmp;
             }
-            float32_t middle = (max + min) / 2;
+
             for(uint16_t i = 0; i < (codec_buf_elements << 1); i++) {
-                dsp_proc_sdr.fft_buf[i].re -= middle;
+                dsp_proc_sdr.fft_buf[i].re -= middle_value;
                 dsp_proc_sdr.fft_buf[i].im = 0;
             }
         }
